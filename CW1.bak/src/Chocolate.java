2f88f5625690bd322c0cbcdeae019a27a068248b
begly <service.alex@begly.co.uk>
2016-02-11 11:52:53 +0000

    Fix dirs

---
+++ CW1.bak/src/Chocolate.java
@@ -0,0 +1,18 @@
+public class Chocolate extends Food {
+
+  @Override
+  public String eaten(Animal animal) {
+    return "animal eats chocolate";
+  }
+
+  @Override
+  public String eaten(Dog dog) {
+    return "dog eats chocolate";
+  }
+
+  @Override
+  public String eaten(Cat cat) {
+    return "cat eats chocolate";
+  } 
+
+}
