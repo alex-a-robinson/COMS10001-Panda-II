9bca6995fa88fbd8df5aab8165ad7f264db29d2c
begly <service.alex@begly.co.uk>
2016-02-23 13:50:18 +0000

    Add CW4

---
+++ src/Dijkstra.java
@@ -0,0 +1,17 @@
+import graph.*;
+
+import java.util.*;
+
+public class Dijkstra {
+
+    public Dijkstra(Graph<Integer, Integer> graph) {
+        //TODO: Using the passed in graph, implement Dijkstras algorithm in this
+        // class.
+    }
+
+    public List<Integer> shortestPath(Integer origin, Integer destination) {
+        //TODO: You should return an ordered list of the node the indecies you
+        // visit in your shortest path from origin to destination.
+    }
+
+}
